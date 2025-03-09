ae411256a8fae003a33842639ddac6168a4cb820
Scurvybelt <90409656+Scurvybelt@users.noreply.github.com>
2025-03-08 23:38:27 -0600

    Login Y registroUser

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from '@/app/servicios/auth.service'
 import { login } from '@/app/store/authentication/authentication.actions'
 import { Component, OnInit, inject } from '@angular/core'
 import {
@@ -6,27 +7,39 @@ import {
   UntypedFormBuilder,
   UntypedFormGroup,
   Validators,
+  
 } from '@angular/forms'
-import { RouterLink } from '@angular/router'
+import { Router, RouterLink } from '@angular/router'
 import { Store } from '@ngrx/store'
+import { AlertsComponent } from '../../ui/alerts/alerts.component'
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap'
+import { CommonModule } from '@angular/common'
+
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FormsModule, ReactiveFormsModule],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule,NgbAlertModule,CommonModule],
   templateUrl: './login.component.html',
   styles: ``,
 })
 export class LoginComponent implements OnInit {
   signInForm!: UntypedFormGroup
   submitted: boolean = false
+  private token: string = ''
+  showAlert: boolean = false
+
+  constructor(private authService: AuthService){
+
+  }
 
   public fb = inject(UntypedFormBuilder)
   public store = inject(Store)
+  private router = inject(Router)
 
   ngOnInit(): void {
     this.signInForm = this.fb.group({
-      email: ['user@demo.com', [Validators.required, Validators.email]],
-      password: ['123456', [Validators.required]],
+      username: ['',[Validators.required]],
+      password: ['',[Validators.required]],
     })
   }
 
@@ -37,11 +50,22 @@ export class LoginComponent implements OnInit {
   login() {
     this.submitted = true
     if (this.signInForm.valid) {
-      const email = this.formValues['email'].value
+      const username = this.formValues['username'].value
       const password = this.formValues['password'].value
+      this.authService.LogInUser({ username: username, password: password}).subscribe((res) => {
+        const { token, user } = res as { token: string, user: string };
+        localStorage.setItem('currentUser', JSON.stringify({ username: user, token: token}));  
+        this.router.navigate(['/index']);
+      },(error) => {
+        console.log(error);
+        this.showAlert = true;
+        setTimeout(() => {
+          this.showAlert = false;
+        }, 6000);
+      })
 
       // Login Api
-      this.store.dispatch(login({ email: email, password: password }))
+      // this.store.dispatch(login({ username: username, password: password }))
     }
   }
 }
